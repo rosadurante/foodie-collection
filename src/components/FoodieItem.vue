6dcfa7cd7dece2575e3c904795db1b124c05633d
Rosa Durante <rosa.durante@helixbyhl.com>
2025-08-11 10:43:41 +0200

    Fix group hover behaviour

--- src/components/FoodieItem.vue
+++ src/components/FoodieItem.vue
@@ -9,8 +9,8 @@
 
 <template>
       <div className="relative flex flex-col items-center justify-start rounded-lg p-2 h-[450px] overflow-hidden bg-teal-700 group">
-        <div className="w-full h-full absolute top-0 left-0 bg-teal-700 hover:opacity-0 transition duration-800"></div>
-        <div className="w-full h-full  absolute top-0 left-0 bg-center bg-cover bg-no-repeat opacity-70 hover:opacity-100 bg-black/50 duration-800 transition hover:scale-120" :style="{ backgroundImage: `url(${props.foodie.src})` }"></div>
+        <div className="w-full h-full absolute top-0 left-0 md:bg-teal-700 group-hover:md:opacity-0 transition duration-800"></div>
+        <div className="w-full h-full  absolute top-0 left-0 bg-center bg-cover bg-no-repeat opacity-70 group-hover:opacity-100 bg-black/50 duration-800 transition group-hover:scale-120" :style="{ backgroundImage: `url(${props.foodie.src})` }"></div>
         <div className="absolute top-50 left-0 w-full duration-800 p-2 items-center opacity-0 invisible transition-opacity ease-in-out
            group-hover:opacity-100 group-hover:visible" >
         <h2 class="text-white text-4xl font-bold uppercase text-center p-2">{{ props.foodie.title }}</h2>
