121c66ab37428caa314c325eab720e914d7f4fcb
Rosa Durante <rosa.durante@helixbyhl.com>
2025-08-27 18:36:26 +0200

    minor fixes

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@ import FoodieDialog from '@/components/FoodieDialog.vue'
 <template>
   <div className="py-4">
   <header className="text-white flex flex-col items-end justify-end">
-    <a href="https://www.rosadurante.com/" className="group flex flex-row items-center justify-center relative text-2xl font-bold py-2 px-4 bg-teal-700 text-teal-900 m-4 transition-transform hover:translate-x-0 translate-x-32 duration-800 ">
-      <span className="text-teal-700 group-hover:text-white transition duration-800 text-sm font-roboto">Back to home page</span></a>
+    <a href="https://www.rosadurante.com/" className="group flex flex-row items-center justify-center relative text-2xl font-bold py-2 px-4 bg-teal-700 text-teal-900 my-4 md:transition-transform md:hover:translate-x-0 md:translate-x-32 md:duration-800 ">
+      <span className="text-white md:text-teal-700 md:group-hover:text-white md:transition md:duration-800 text-sm font-roboto">Back to home page</span></a>
   </header>
 
   <main className="flex flex-col items-center justify-center">
--- src/components/FoodieDialog.vue
+++ src/components/FoodieDialog.vue
@@ -16,9 +16,18 @@ console.log(isOpen.value)
 </script>
 
 <template>
+  <transition
+      enter-active-class="transition-opacity duration-500"
+      enter-from-class="opacity-0"
+      enter-to-class="opacity-100"
+      leave-active-class="transition-opacity duration-500"
+      leave-from-class="opacity-100"
+      leave-to-class="opacity-0"
+    >
   <div v-if="isOpen" class="fixed inset-0 bg-black/50 bg-opacity-50 flex items-center justify-center z-50 " @click.self="handleClose">
     <div class="bg-white rounded-lg w-[50%] h-[50%] shadow-lg overflow-y-auto">
       <FoodieDetails :foodie="foodStore.selectedFood" :onClose="handleClose" />
     </div>
   </div>
+  </transition>
 </template>
